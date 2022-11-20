b201e424bb5a1c7c7780ab39c7cf7473944208fe
Patched <kiddingtrust393@gmail.com>
2022-11-21 00:53:10 +0600

    Initial Commit

---
+++ 2.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2.1
+//
+//  Created by Данила Кардашевский on 20.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
