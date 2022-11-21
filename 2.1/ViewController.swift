00217f9ec913513dd510e197ce0aae3cd1f14b6e
Patched <kiddingtrust393@gmail.com>
2022-11-21 13:10:19 +0600

    Event handler for the button

--- 2.1/ViewController.swift
+++ 2.1/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var ToogleButton: UIButton!
+    @IBOutlet var HelloworldLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        HelloworldLabel.isHidden = true
+        ToogleButton.layer.cornerRadius = 10
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func makeButtonAction(_ sender: Any) {
+        
+        if HelloworldLabel.isHidden {
+            HelloworldLabel.isHidden = false
+            ToogleButton.setTitle("Hide Text", for: .normal)
+        }
+        
+        else{
+            HelloworldLabel.isHidden = true
+            ToogleButton.setTitle("Show Text", for: .normal)
+        }
+    }
+    
 }
 
